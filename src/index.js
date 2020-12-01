b09ea8d0029166a3d65906fe258ddd76b3c7a170
Amit Erandole <amit.erandole@gmail.com>
2020-12-01 11:42:50 +0530

    changes semi setttings

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import 'dotenv/config';
+import 'dotenv/config'
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const userCredentials = { firstname: 'Robin' }
+const userDetails = { nationality: 'German' }
 
 const user = {
   ...userCredentials,
   ...userDetails,
-};
+}
 
-console.log(user);
+console.log(user)
 
-console.log(process.env.SOME_ENV_VARIABLE);
+console.log(process.env.SOME_ENV_VARIABLE)
